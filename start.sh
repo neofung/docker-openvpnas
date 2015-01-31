79984b7346fc008055f3ea66e7a83e91092e0fb6
Neo Fung <hack@neofung.org>
2015-01-31 08:34:16 +0000

    update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,16 @@
 # Description: 
 #########################################################################
 
-docker run -p 443:443/tcp -p 943:943/tcp -p 1194:1194/udp -p 10324:10324/udp -d --privileged -t openvpnas:latest /sbin/my_init --enable-insecure-key
+# build image
+docker build -t docker-openvpnas .
+
+# run container
+docker run -p 443:443/tcp -p 943:943/tcp -p 1194:1194/udp -p 10324:10324/udp -d --privileged -t docker-openvpnas:latest /sbin/my_init --enable-insecure-key
+
+# install docker-ssh
+curl --fail -L -O https://github.com/phusion/baseimage-docker/archive/master.tar.gz
+tar xzf master.tar.gz
+sudo ./baseimage-docker-master/install-tools.sh
+rm -rf baseimage-docker-master
+rm -f master.tar.gz base
+
